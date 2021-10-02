ac8e86203535334a4c6b728159a3e564b94c21c5
dimastatz <dima.statz@gmail.com>
2021-10-02 21:48:34 +0300

    add implementation

--- data-process/flumenz/src/main/scala/dimastatz/flumenz/cdnquality/CdnQualityPipeline.scala
+++ data-process/flumenz/src/main/scala/dimastatz/flumenz/cdnquality/CdnQualityPipeline.scala
@@ -1,8 +1,9 @@
 package dimastatz.flumenz.cdnquality
 
+import java.sql.Timestamp
 import dimastatz.flumenz.Pipeline
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions._
 
 object CdnQualityPipeline extends Pipeline {
   override def getName: String = "CdnQuality"
@@ -10,9 +11,17 @@ object CdnQualityPipeline extends Pipeline {
   override def getPartitions: List[String] = List("exec_dt")
 
   override def query(df: DataFrame): DataFrame = {
+    val convertedTimestampUdf = udf(convertTimestamp _)
     df
       .select("timestamp", "value", "topic")
-      .filter(col("topic") === "cdn")
-      .select("rewritten_path", "status_code")
+      .filter(col("topic") === "cdnlogs")
+      .select("value", "timestamp")
+      .withColumn("exec_dt", convertedTimestampUdf(col("timestamp")))
+      .drop("timestamp")
+  }
+
+  def convertTimestamp(ts: Timestamp): String = {
+    import dimastatz.flumenz.utilities.Extensions._
+    ts.convertTimestamp()
   }
 }
--- data-process/flumenz/src/main/scala/dimastatz/flumenz/utilities/Extensions.scala
+++ data-process/flumenz/src/main/scala/dimastatz/flumenz/utilities/Extensions.scala
@@ -2,6 +2,10 @@ package dimastatz.flumenz.utilities
 
 import org.apache.spark.sql.DataFrame
 
+import java.sql.Timestamp
+import java.util.TimeZone
+import java.text.SimpleDateFormat
+
 object Extensions {
   implicit class KafkaDataFrame(df: DataFrame) {
     def getKafkaLabels: DataFrame = {
@@ -11,6 +15,14 @@ object Extensions {
         "CAST(value AS STRING)",
         "CAST(timestamp AS TIMESTAMP)"
       )
+    }
+  }
+
+  implicit class TimestampExtensions(ts: Timestamp) {
+    def convertTimestamp(pattern: String = "yyyyMMddHH"): String = {
+      val dateFormat = new SimpleDateFormat(pattern)
+      dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+      dateFormat.format(ts)
     }
   }
 }
