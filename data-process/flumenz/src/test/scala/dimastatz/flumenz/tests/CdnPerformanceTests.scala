ba4b0d0c5f6a2ab8089f9032000ce3bebabd352b
dimastatz <dima.statz@gmail.com>
2021-10-05 23:43:10 +0300

    add coverage

--- data-process/flumenz/src/test/scala/dimastatz/flumenz/tests/CdnPerformanceTests.scala
+++ data-process/flumenz/src/test/scala/dimastatz/flumenz/tests/CdnPerformanceTests.scala
@@ -3,10 +3,13 @@ package dimastatz.flumenz.tests
 import scala.io.Source
 import org.apache.spark.sql._
 import com.typesafe.config.Config
+
 import scala.collection.JavaConverters._
 import org.scalatest.funsuite.AnyFunSuite
 import dimastatz.flumenz.{Pipeline, Pipelines}
 import dimastatz.flumenz.cdnquality.CdnQualityPipeline
+
+import java.util.UUID
 
 class CdnPerformanceTests extends AnyFunSuite with SparkTest {
   val config: Config = getConfig
@@ -19,14 +22,18 @@ class CdnPerformanceTests extends AnyFunSuite with SparkTest {
 
   test(testName = "testCdnQualityFlow") {
     import session.sqlContext.implicits._
+    import dimastatz.flumenz.utilities.Extensions._
     val lines = Source.fromResource("cdn_log.json").getLines().toList
 
     val df = lines
-      .map(l => (toTimestamp("2021-01-01T00:20:28"), "cdnlogs", l))
-      .toDF("timestamp", "topic", "value")
+      .map(l => (UUID.randomUUID().toString, toTimestamp("2021-01-01T00:20:28"), "cdnlogs", l))
+      .toDF("key", "timestamp", "topic", "value")
+      .getKafkaLabels
 
-    df.show()
     assert(df.count() == 9)
+    assert(df.columns.length == 4)
+    assert(CdnQualityPipeline.getName == "CdnQuality")
+    assert(CdnQualityPipeline.getPartitions.contains("exec_dt"))
 
     val result = CdnQualityPipeline.query(df)
     result.show()
