ebb9acfb219232f3cb8fc6ee19fcae6d0802d981
dimastatz <dima.statz@gmail.com>
2022-05-11 23:19:39 +0300

    add kafka producer

--- video-ingest/gateway/src/app.py
+++ video-ingest/gateway/src/app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+import os
+import logging
+from kafka import KafkaProducer
+from flask import Flask, request
+
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+def health():
+    try:
+        test_producer = create_producer()
+        test_producer.close()
+        return '<h1>Hello from Video Ingest</h2>'
+    except Exception as e:
+        logging.error(f'Error creating Video Ingest {e}')
+        return 'Error creating Video Ingest'
+
+
+@app.route('/frame/<beam_id>/<slice_id>/<frame_id>', methods=['POST']) 
+def send_frame(beam_id, slice_id, frame_id):
+    try:
+        logging.info('Sending frame {beam_id} {slice_id} {frame_id}')
+        producer = create_producer()
+        producer.send(topic=beam_id, value=request.data)
+    except Exception as e:
+        logging.error(f'Error sending frame {e}')
+
+
+def create_producer(servers=os.environ.get('kafka_servers', '')):
+    return KafkaProducer(bootstrap_servers=servers)
 
 
 if __name__ == "__main__":
