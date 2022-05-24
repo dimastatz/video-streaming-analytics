5495e9fd0229d13f4ac8551da0d6dcb5e2fa9e58
dimastatz <dima.statz@gmail.com>
2022-05-24 23:00:56 +0300

    test origin_set

--- video-ingest/video-origin-poc/cdn_origin/origin_set.py
+++ video-ingest/video-origin-poc/cdn_origin/origin_set.py
@@ -6,12 +6,13 @@ class OriginSet:
     def __init__(self, origins: List[Origin]):
         self.trigger = threading.Event()
         self.origins: List[Origin] = origins
+        [o.add_all_done_callback(lambda: self.trigger.set()) for o in origins]
         
     def upload(self, dest_name: str, data: bytes, job_types: str, info: None):
         [o.upload(dest_name, data, job_types, info) for o in self.origins]
         
-    def wait(self, timeout: float):
-        next(o.wait(timeout) for o in self.origins)
-
+    def wait(self, timeout: float) -> bool:
+        return self.trigger.wait(timeout)
+        
     def close(self):
         [o.close() for o in self.origins]
