7697e57795f0a68a5c9b62d697d210e0f617f899
dimastatz <dima.statz@gmail.com>
2022-05-24 23:07:30 +0300

    test origin_set

--- video-ingest/video-origin-poc/tests/test_origin_set.py
+++ video-ingest/video-origin-poc/tests/test_origin_set.py
@@ -13,12 +13,24 @@ class TestOriginSet(unittest.TestCase):
         print('upload_dummy_2', dest_name, data, job_types, info)
         time.sleep(0.1)
 
+    def upload_dummy_3(self, dest_name: str, data: bytes, job_types: str, info: None):
+        print('upload_dummy_2', dest_name, data, job_types, info)
+        time.sleep(0.5)
+
     def test_tasks_success(self):
         x = OriginSet([Origin(self.upload_dummy_1), Origin(self.upload_dummy_2)])
         x.upload('dummy', b'some_data', 'type_a', 'info')
         assert(x.wait(0.05) == False)
         assert(x.wait(0.09) == True)
         assert(x.wait(0.01) == True)
+        x.close()
+
+
+    def test_one_completed(self):
+        x = OriginSet([Origin(self.upload_dummy_1), Origin(self.upload_dummy_3)])
+        x.upload('dummy', b'some_data', 'type_a', 'info')
+        assert(x.wait(0.05) == False)
+        assert(x.wait(0.1) == True)
         x.close()
     
 
