f71b575eb970f397e97f3c542a1ca6387dd49044
dimastatz <dima.statz@gmail.com>
2022-05-24 21:52:16 +0300

    add unitests

--- video-ingest/video-origin-poc/cdn_origin/origin.py
+++ video-ingest/video-origin-poc/cdn_origin/origin.py
@@ -6,23 +6,27 @@ from concurrent.futures import ThreadPoolExecutor, Future, wait
 class Origin:
     def __init__(self, upload_handler: Callable, workers: int = 16) -> None:
         self.results = Queue()
+        self.all_done_callback = None
         self.upload_handler = upload_handler
         self.pool = ThreadPoolExecutor(workers)
         
     def upload(self, dest_name: str, data: bytes, job_types: str, info: None):
         future: Future = self.pool.submit(self.upload_handler, dest_name, data, job_types, info)
-        future.add_done_callback(self.all_done_callback)
+        future.add_done_callback(self.done_callback)
         self.results.put(future)
         
     def wait(self, timeout: float):
-        features = list(self.results)
+        features = list(self.results.queue)
         result = wait(features, timeout=timeout)
         return result
 
-    def all_done_callback(self, handler: Callable):
-        has_running_tasks = any(f for f in list(self.results) if f.running())
-        if not has_running_tasks:
-            handler()
+    def add_all_done_callback(self, callback: Callable):
+        self.all_done_callback = callback
+
+    def done_callback(self, _: Future):
+        has_running_tasks = any(f for f in list(self.results.queue) if f.running())
+        if not has_running_tasks and self.all_done_callback:
+            self.all_done_callback()
 
     def close(self):
         self.pool.shutdown()
--- video-ingest/video-origin-poc/tests/test_origin.py
+++ video-ingest/video-origin-poc/tests/test_origin.py
@@ -1,10 +1,25 @@
+import time
 import unittest
-import 
+from cdn_origin.origin import Origin
 
 
 class TestOrigin(unittest.TestCase):
+    def upload_dummy(self, dest_name: str, data: bytes, job_types: str, info: None):
+        print(dest_name, data, job_types, info)
+        time.sleep(1)
+
+
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        x = Origin(self.upload_dummy)
+        x.upload('dummy1', b'some_data', 'type_a', 'info')
+        x.upload('dummy2', b'some_data', 'type_a', 'info')
+        
+        finished, running = x.wait(0.5)
+        self.assertEqual(len(running), 2)
+        
+        finished, running = x.wait(2)
+        self.assertEqual(len(finished), 2)
+        
 
 
 
