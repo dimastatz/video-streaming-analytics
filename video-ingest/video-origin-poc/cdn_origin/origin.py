7852e495b5b210941070978df23663bcc750b6d3
dimastatz <dima.statz@gmail.com>
2022-05-24 22:09:02 +0300

    add private methods

--- video-ingest/video-origin-poc/cdn_origin/origin.py
+++ video-ingest/video-origin-poc/cdn_origin/origin.py
@@ -9,10 +9,15 @@ class Origin:
         self.all_done_callback = None
         self.upload_handler = upload_handler
         self.pool = ThreadPoolExecutor(workers)
+
+    def __done_callback(self, _: Future):
+        has_running_tasks = any(f for f in list(self.results.queue) if f.running())
+        if not has_running_tasks and self.all_done_callback:
+            self.all_done_callback()
         
     def upload(self, dest_name: str, data: bytes, job_types: str, info: None):
         future: Future = self.pool.submit(self.upload_handler, dest_name, data, job_types, info)
-        future.add_done_callback(self.done_callback)
+        future.add_done_callback(self.__done_callback)
         self.results.put(future)
         
     def wait(self, timeout: float):
@@ -22,11 +27,6 @@ class Origin:
 
     def add_all_done_callback(self, callback: Callable):
         self.all_done_callback = callback
-
-    def done_callback(self, _: Future):
-        has_running_tasks = any(f for f in list(self.results.queue) if f.running())
-        if not has_running_tasks and self.all_done_callback:
-            self.all_done_callback()
 
     def close(self):
         self.pool.shutdown()
--- video-ingest/video-origin-poc/tests/test_origin.py
+++ video-ingest/video-origin-poc/tests/test_origin.py
@@ -6,20 +6,34 @@ from cdn_origin.origin import Origin
 class TestOrigin(unittest.TestCase):
     def upload_dummy(self, dest_name: str, data: bytes, job_types: str, info: None):
         print(dest_name, data, job_types, info)
-        time.sleep(1)
+        time.sleep(0.1)
 
-
-    def test_upper(self):
+    def test_tasks_success(self):
         x = Origin(self.upload_dummy)
         x.upload('dummy1', b'some_data', 'type_a', 'info')
         x.upload('dummy2', b'some_data', 'type_a', 'info')
     
-        finished, running = x.wait(0.5)
+        finished, running = x.wait(0.01)
         self.assertEqual(len(running), 2)
         finished, running = x.wait(2)
         self.assertEqual(len(finished), 2)
         x.close()
         
+    def test_callback(self):
+        callback_finished = False
 
+        def all_done_callback():
+           nonlocal callback_finished
+           callback_finished = True
+
+        x = Origin(self.upload_dummy)
+        x.upload('dummy1', b'some_data', 'type_a', 'info')
+        time.sleep(0.11)
+        assert(callback_finished == False)
+
+        x.add_all_done_callback(all_done_callback)
+        x.upload('dummy1', b'some_data', 'type_a', 'info')
+        time.sleep(0.11)
+        assert(callback_finished == True)
 
 
