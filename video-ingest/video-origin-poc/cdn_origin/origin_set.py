c9846f9a4fbf7315429d858d219943ff29ba75c6
dimastatz <dima.statz@gmail.com>
2022-05-24 23:00:45 +0300

    test origin_set

--- video-ingest/video-origin-poc/cdn_origin/origin_set.py
+++ video-ingest/video-origin-poc/cdn_origin/origin_set.py
@@ -1,10 +1,12 @@
+import threading
 from typing import List
 from cdn_origin.origin import Origin
 
 class OriginSet:
     def __init__(self, origins: List[Origin]):
+        self.trigger = threading.Event()
         self.origins: List[Origin] = origins
-
+        
     def upload(self, dest_name: str, data: bytes, job_types: str, info: None):
         [o.upload(dest_name, data, job_types, info) for o in self.origins]
         
